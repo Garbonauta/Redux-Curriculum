74dfcfdf5496df3db95e2c1a6d2793f63ece712a
Garbonauta <eduardo.moreno.j@gmail.com>
2017-11-16 22:19:53 -0600

    Finished Redux Curriculumn according to Tyler McGinnis. This uses:  React 16  React Router v4  Firebase Firestore
     All the fun stuff. It works well. Had it updating in real time for firestore
     on data updates however the data would update slowly as it made it through the changes.
     Can likely make a hybrid approach but it may be time to go on my own.

--- App/config/constants.js
+++ App/config/constants.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
-const config = {
-  apiKey: 'AIzaSyCA6_ioLNbCuT3tLa1yQO9uy44oPb4qqCs',
-  authDomain: 'redux-curriculum.firebaseapp.com',
-  databaseURL: 'https://redux-curriculum.firebaseio.com',
-  projectId: 'redux-curriculum',
-  storageBucket: 'redux-curriculum.appspot.com',
-  messagingSenderId: '289082769346',
-}
+
 
 firebase.initializeApp(config)
 
